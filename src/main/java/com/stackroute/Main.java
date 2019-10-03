c9b1e631048e17dd02ec2f7bd2b3fe0f6d6354c0
praveenawasthi01091 <praveenawasthi01091@gmail.com>
2019-10-03 12:47:58 +0530

    "Bean-Post-processor task"

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -1,7 +1,6 @@
 package com.stackroute;
 
 import com.stackroute.config.Config;
-import com.stackroute.demo.BeanLifeCycleDemo;
 import com.stackroute.demo.BeanPostProcessorDemo;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -13,8 +12,8 @@ public class Main
     {
 
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(Config.class);
-        BeanPostProcessorDemo bean=applicationContext.getBean(BeanLifeCycleDemo.class);
-        System.out.println(beanLifeCycleDemo);
+        BeanPostProcessorDemo beanPostProcessorDemo=applicationContext.getBean(BeanPostProcessorDemo.class);
+        System.out.println(beanPostProcessorDemo);
         AnnotationConfigApplicationContext axt=(AnnotationConfigApplicationContext) applicationContext;
         axt.close();
     }
--- src/main/java/com/stackroute/config/Config.java
+++ src/main/java/com/stackroute/config/Config.java
@@ -1,25 +1,13 @@
 package com.stackroute.config;
 
 import com.stackroute.demo.BeanPostProcessorDemo;
-import com.stackroute.domain.Actor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan("com.stackroute.domain com.stackroute.demo")
+@ComponentScan("com.stackroute.demo")
 public class Config {
-    @Bean
-    public Actor actor()
-    {
-        return new Actor("SRK","Male",50);
-    }
-
-    @Bean
-    public Actor actor1()
-    {
-        return new Actor("Salman","Male",50);
-    }
 
    @Bean
     public BeanPostProcessorDemo beanPostProcessor()
