6a3f3b74efb1950f28864a365f22e8a28cf4d953
praveenawasthi01091 <praveenawasthi01091@gmail.com>
2019-10-03 11:33:41 +0530

    "Aware-interface task"

--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -1,20 +1,28 @@
 package com.stackroute.domain;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.BeanNameAware;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
 @Scope("prototype")
-public class Movie {
+public class Movie implements BeanNameAware , BeanFactoryAware, ApplicationContextAware {
+
     private Actor actor;
+    private ApplicationContext applicationContext;
 
     @Autowired
     public Movie(Actor actor) {
         this.actor = actor;
     }
-    public Movie()
-    {
+
+    public Movie() {
 
     }
 
@@ -31,5 +39,24 @@ public class Movie {
         return "Movie{" +
                 "actor=" + actor +
                 '}';
+    }
+
+    @Override
+    public void setBeanName(String s) {
+        System.out.println("BeanName: ------->"+ s);
+
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println("beanFactory: ------>"+ beanFactory);
+
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println("applicationContext -------->"+ applicationContext);
+        this.applicationContext=applicationContext;
+
     }
 }
